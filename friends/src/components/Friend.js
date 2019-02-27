ea5cdc6f2236aa1a39bc653cecf067dfd5cb4fd8
AAsriyan <aasriyan@protonmail.com>
2019-02-27 15:34:20 -0500

    Built out Friends List component and able to show it on the webpage

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Friend = props => {
+  return (
+    <div className="friend-card">
+      <h2>Name: {props.friend.name}</h2>
+      <p>Age: {props.friend.age}</p>
+      <p>Email: {props.friend.email}</p>
+    </div>
+  );
+};
+
+export default Friend;
